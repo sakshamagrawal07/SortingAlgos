e942de0c15452f8a34df41b21abf6f7ca9211399
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 15:00:09 +0530

    small changes

--- quickSort.c
+++ quickSort.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int partition(int arr[], int low, int high) {
+int partition(int arr[], int low, int high)
+{
     int pivot = arr[high];
     int i = (low - 1);
 
-    for (int j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
             i++;
             int temp = arr[i];
             arr[i] = arr[j];
@@ -19,22 +22,27 @@ int partition(int arr[], int low, int high) {
     return i + 1;
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
 }
 
-int main() {
+int main()
+{
     int arr[] = {10, 7, 8, 9, 1, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
 
