02ef8d0e7d08806b9357e9d4cbd3c809b0b1f943
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:41:24 +0530

    small change

--- bubbleSort.c
+++ bubbleSort.c
@@ -8,7 +8,6 @@ void bubbleSort(int arr[], int n)
         {
             if (arr[j] > arr[j + 1])
             {
-                // Swap arr[j] and arr[j+1]
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
