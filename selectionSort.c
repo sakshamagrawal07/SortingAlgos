f8cc2b5a98678976240d9a9b9e7b9c774e2554b7
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:44:52 +0530

    main function added

--- selectionSort.c
+++ selectionSort.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
         int minIdx = i;
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIdx]) {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIdx])
+            {
                 minIdx = j;
             }
         }
@@ -14,9 +18,27 @@ void selectionSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+int main()
+{
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
 }
