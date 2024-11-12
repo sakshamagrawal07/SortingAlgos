84deab9d01f6ddd4c81269799f5760ad8a76d830
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:48:22 +0530

    main function added

--- insertionSort.c
+++ insertionSort.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
         int key = arr[i];
         int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
+        while (j >= 0 && arr[j] > key)
+        {
             arr[j + 1] = arr[j];
             j--;
         }
@@ -13,9 +16,27 @@ void insertionSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    printArray(arr, n);
+
+    insertionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
 }
