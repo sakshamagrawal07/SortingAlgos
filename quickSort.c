841e7c8d44313eb181f87025e744d38d839f2fb7
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:56:32 +0530

    print function added

--- quickSort.c
+++ quickSort.c
@@ -27,3 +27,9 @@ void quickSort(int arr[], int low, int high) {
     }
 }
 
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
