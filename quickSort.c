3fb094d1a55444d3054e56f6e6ec2c18d7723870
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:58:16 +0530

    main function added

--- quickSort.c
+++ quickSort.c
@@ -33,3 +33,18 @@ void printArray(int arr[], int n) {
     }
     printf("\n");
 }
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: ");
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
