944c7afe6e16a621f1b7de218fb41d2a2585727e
Saksham Agrawal <agsaksham07@gmail.com>
2024-11-12 14:37:45 +0530

    print function added

--- bubbleSort.c
+++ bubbleSort.c
@@ -12,3 +12,10 @@ void bubbleSort(int arr[], int n) {
         }
     }
 }
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
